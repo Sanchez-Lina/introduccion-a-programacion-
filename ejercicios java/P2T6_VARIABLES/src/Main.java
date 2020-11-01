998e9945b7b002e7c6f79d1bec7c915a96e688ac
LINA <l>
2020-11-01 12:05:07 -0500

    P2T6
    P2T6

---
+++ ejercicios java/P2T6_VARIABLES/src/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("---------------------------------");
+        System.out.println(" P2T6 variables. By lina Sanchez");
+        System.out.println("---------------------------------");
+         int num1=20, num2=25;
+         double num3= 23.9866, num4= 5987.34576543;
+
+         System.out.println("int numbers"+num1+"||"+num2);
+         System.out.println("double number"+ num3+ "||"+ num4);
+    }
+}
